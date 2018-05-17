fa5edb51dff1e58ed5d0cfb64aa31dca80621753
lvjingshuai <984133382@qq.com>
2018-05-17 15:50:40 +0800

    projectList

---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+window.onload = function () {
+    ReactDOM.render(<div>aaaa</div>,document.getElementById('root'))
+}
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+module.exports={
+
+    entry: './index.js',
+  
+    output: {
+  
+      path: __dirname ,
+  
+      filename: 'bundle.js'
+  
+    },
+  
+    devtool: 'source-map',
+  
+    module: {
+  
+      loaders: [
+  
+        {test:/\.css$/, loader: 'style!css'},
+  
+        {test:/\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+  
+      ]
+  
+    }
+  
+  };
